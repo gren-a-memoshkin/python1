29ca3d8d697d34b0df74fbe89ee3264861825d43
gren-a-memoshkin <mmozpp@gmail.com>
2024-04-24 20:49:15 +0500

    init

--- task8.py
+++ task8.py
@@ -1,8 +1,25 @@
 import random
 
 list1 = []
-for k in range (10):
-    list1.append(random.randint(-10,10))
-    print (list1)
+list2 = []
+list3 = []
+list4 = []
 
+for k in range (5):
+    list1.append(random.randint(-5,5)*2)
+for i in range (3):
+    list2.append(random.randint (-5,5)*2)
+
+for i in list1:
+    if i >= 0:
+        list3.append(i)
+    elif i <= 0:
+        list4.append(i)
+for i in list2:
+    if i >= 0:
+        list3.append(i)
+    elif i <= 0:
+        list4.append(i)
+        
+print (list1, list2, list3, list4)
    
