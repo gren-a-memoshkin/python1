0594e1bfe5aa934e316a75b3010ced4487aacbda
gren-a-memoshkin <mmozpp@gmail.com>
2024-04-24 15:31:52 +0500

    plus task11

--- task11.py
+++ task11.py
@@ -1,8 +1,10 @@
 #проверка формата телефонного номера re
 import re
-phone_numbers = [input("введите номер телефона: ")]
+phone_numbers = [input("введите номер телефона или номер авто: ")] or "номер не введен"
 for number in phone_numbers:
     if re.match(r'[78][0-9]{10}', number) and len(number) == 11:
-        print("соответсвует формату")
+        print("соответсвует формату телефонного номера")
+    elif re.match(r"[A-Z]\d{3}[A-Z]{2}\d{2,3}", number) and len(number) ==9:
+        print ("соответсвует номеру автомобиля")
     else:
         print("не соответсвует формату")
